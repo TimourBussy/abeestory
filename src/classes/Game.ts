8ba8d0564f7bb20d69359772e6638f1e89abf8b6
TimourBussy <timourjbanov@gmail.com>
2026-05-16 22:28:22 +0200

    Little improvement

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -223,7 +223,7 @@ export class Game {
     );
     this._npcs.push(
       new NPC(
-        200,
+        5000,
         "Professeur Mellis",
         [
           "Félicitations petite abeille ! Tu es arrivée au bout de ton voyage.",
@@ -273,11 +273,11 @@ export class Game {
 
     const [sky, bee, ground, textbox, soundBtn, ...npcImgs] = await Promise.all(
       [
-        this.loadImage(`${import.meta.env.BASE_URL}/sprites/sky.png`),
-        this.loadImage(`${import.meta.env.BASE_URL}/sprites/bee.png`),
-        this.loadImage(`${import.meta.env.BASE_URL}/sprites/ground.png`),
-        this.loadImage(`${import.meta.env.BASE_URL}/sprites/textbox.png`),
-        this.loadImage(`${import.meta.env.BASE_URL}/sprites/sound_btn.png`),
+        this.loadImage(`/sprites/sky.png`),
+        this.loadImage(`/sprites/bee.png`),
+        this.loadImage(`/sprites/ground.png`),
+        this.loadImage(`/sprites/textbox.png`),
+        this.loadImage(`/sprites/sound_btn.png`),
         ...npcSrcs.map((src) => this.loadImage(src)),
       ],
     );
@@ -311,7 +311,7 @@ export class Game {
       const img = new Image();
       img.onload = () => resolve(img);
       img.onerror = () => reject(new Error(`Failed to load image: ${src}`));
-      img.src = src;
+      img.src = `${import.meta.env.BASE_URL}${src}`;
     });
   }
 
